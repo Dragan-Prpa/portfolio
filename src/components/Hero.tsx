f88b53e36df1df3b3dd7e0c78f5a9abd13aeb781
Dragan-Prpa <prpa.dev@gmail.com>
2026-05-17 12:00:21 +0200

    revert change example

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,6 @@ export default function Hero() {
       <h2 className="text-5xl md:text-6xl font-bold mb-4 text-gray-900 dark:text-white">Hi, I'm Dragan</h2>
       <p className="text-lg md:text-xl text-gray-600 dark:text-gray-300 max-w-2xl">
         I am a full-stack developer, programming teacher, hobby game developer and a student.
-        blblbanfjobfjab
       </p>
     </section>
   )
