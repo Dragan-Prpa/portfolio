eb7cc9e33c315a27fa7b0976ea5bfbe21db57e2b
Dragan Prpa <prpa.dev@gmail.com>
2026-06-04 22:22:00 +0200

    fix the card size

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,26 +5,41 @@ interface ProjekatProps {
   description: string;
   technologyUsed: string;
   image: string;
-  link: string; 
+  link: string;
 }
 
-const Projekat: React.FC<ProjekatProps> = ({ title, description,technologyUsed, image, link }) => {
+const Projekat: React.FC<ProjekatProps> = ({
+                                             title,
+                                             description,
+                                             technologyUsed,
+                                             image,
+                                             link,
+                                           }) => {
   return (
-    <a
-      href={link}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="group block w-80 p-4 bg-cyberbg border border-cyberpurple rounded-md shadow-[0_0_10px_rgba(170,59,255,0.6)] hover:shadow-[0_0_20px_rgba(170,59,255,0.9)] transition-shadow duration-200"
-    >
-      <img
-          src={image}
-          alt={title}
-          className="w-full h-48 object-cover rounded-md mb-3"
-      />
-      <h3 className="text-cyberpink text-xl font-bold mb-1">{title}</h3>
-      <p className="text-cyberblue text-sm">{description}</p>
-      <p className="text-cyberblue text-sm">{technologyUsed}</p>
-    </a>
+      <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="group flex flex-col h-full w-full p-4 bg-cyberbg border border-cyberpurple rounded-md shadow-[0_0_10px_rgba(170,59,255,0.6)] hover:shadow-[0_0_20px_rgba(170,59,255,0.9)] transition-shadow duration-200"
+      >
+        <img
+            src={image}
+            alt={title}
+            className="block w-full h-48 object-cover rounded-md mb-3"
+        />
+
+        <h3 className="text-cyberpink text-xl font-bold mb-1">
+          {title}
+        </h3>
+
+        <p className="text-cyberblue text-sm">
+          {description}
+        </p>
+
+        <p className="text-cyberblue text-sm mt-auto pt-2">
+          {technologyUsed}
+        </p>
+      </a>
   );
 };
 
